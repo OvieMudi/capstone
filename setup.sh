d8ec1463d9ecc920a4653dc228e065a433cc5ffc
OvieMudi <oviisaacs@gmail.com>
2020-01-29 16:14:00 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 export FLASK_APP=app.py
 export FLASK_ENV=development
 
-# NOTE: first create a config.env file.
-# See config.env.example for required content
+# NOTE: first create a .env file.
 
 python manage.py db init
 python manage.py db migrate
